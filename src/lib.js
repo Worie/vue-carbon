27b385b07b155a711868ed685572f0a7875fda55
Wojciech Połowniak <wojtek.polowniak@functionite.com>
2018-04-01 20:06:21 +0200

    get ready for first npm push

--- src/lib.js
+++ src/lib.js
@@ -2,12 +2,12 @@ import '../node_modules/carbon-components/css/carbon-components.css';
 
 import Button from './components/button';
 import Accordion from './components/accordion';
-import Breadcrumbs from './components/breadcrumbs';
+import Breadcrumb from './components/breadcrumb';
 
 function install(Vue) {
   Vue.component(Button.name, Button);
   Vue.component(Accordion.name, Accordion);
-  Vue.component(Breadcrumbs.name, Breadcrumbs);
+  Vue.component(Breadcrumb.name, Breadcrumb);
 }
 
 export default {
